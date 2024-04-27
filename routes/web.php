7fc3f5b2622f042bccfb10a71badf5bc1167b7cd
Dennis Martin <demargin2409@gmail.com>
2024-04-27 02:31:20 -0500

    New code lines

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+//By deafutl Route
+/* Route::get('/', function () {
     return view('welcome');
 });
+ */
+
+ /**
+  * Here an example when we use 
+  */
+/* 
+ Route::get('/', function(){
+    return 'Hola como estas , todo bien ?';
+ }); */
+
+ Route::get('/', [WelcomeController::class, 'index']);
